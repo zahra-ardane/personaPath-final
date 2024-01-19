853acec14e02c327bf47622fda65ab72614d8ded
zahra-ardane <zahraardane2001@gmail.com>
2024-01-19 14:50:09 +0330

    workflow api almsot completed

--- src/pages/workflow/addWorkflow.js
+++ src/pages/workflow/addWorkflow.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import { Box, Container, Stack, Typography, Unstable_Grid2 as Grid } from '@mui/material';
 import { Layout as DashboardLayout } from 'src/layouts/dashboard/layout';
-import { AddWorkflow } from '../../sections/workflow/addWorkFlowComp'
+import { AddWorkflow } from '../../sections/workflow/components/addWorkFlowComp'
 
 const Page = () => (
   <>
